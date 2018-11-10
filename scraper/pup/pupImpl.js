7be1c69388efe3f49d00a59dd686aba753ea3638
fernandok4 <fernando.kanashiro98@globomail.com>
2018-11-10 18:03:52 -0200

    realizando testes

--- scraper/pup/pupImpl.js
+++ scraper/pup/pupImpl.js
@@ -1,13 +1,21 @@
 const pup = require('puppeteer')
 const dao = require('../dao/pupDAO')
+const soloPup = require('./pupSoloImpl')
+const duelPup = require('./pupDuelImpl')
 const CARDS_DATABASE = "https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sess=1&keyword=&stype=1&ctype=&starfr=&starto=&pscalefr=&pscaleto=&linkmarkerfr=&linkmarkerto=&link_m=2&atkfr=&atkto=&deffr=&defto=&othercon=1&rp=100&page="
+var qtdWebSites = 0;
 
 async function run() {
     const browser = await pup.launch()
     const page = await browser.newPage()
     await page.goto(CARDS_DATABASE)
-    await getAllCards(page)
-    await quit(browser)
+    // await getAllCards(page)
+    getPrices(browser)
+}
+
+function getPrices(browser){
+    getSoloPrices(browser)
+    getDuelShopPrices(browser)
 }
 
 async function quit(browser) {
@@ -18,7 +26,6 @@ async function getAllCards(page) {
     let listCards = []
     dao.clearCards()
     for (let i = 1; i < 200; i++) {
-        console.log(i)
         try {
             await page.goto(CARDS_DATABASE + String(i))
             const cardNames = await page.evaluate(() => [...document.querySelectorAll('.box_card_name')].map(elem => elem.innerText))
@@ -38,6 +45,32 @@ async function getAllCards(page) {
         }
     }
     dao.insertCards(listCards)
+}
+
+async function getSoloPrices(browser){
+    try {
+        const page = await browser.newPage()
+        console.log('uHUlll')    
+        qtdWebSites++
+        if(qtdWebSites == 2){
+            await quit(browser)
+        }
+    } catch (e){
+        console.log("alguma cosia deu errada")
+    }
+}
+
+async function getDuelShopPrices(browser){
+    try {
+        const page = await browser.newPage()
+        console.log('uHUlll')    
+        qtdWebSites++
+        if(qtdWebSites == 2){
+            await quit(browser)
+        }
+    } catch (e){
+        console.log("alguma cosia deu errada")
+    }
 }
 
 run()
