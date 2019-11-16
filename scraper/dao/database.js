df7895cd227e6970449d99a23d15aac98038d040
fernandok4 <fernando.kanashiro98@globomail.com>
2019-11-16 13:05:40 -0200

    Alterando o gitignore e fazendo download das imagens

---
+++ scraper/dao/database.js
@@ -0,0 +1,30 @@
+const mysql = require( 'mysql' );
+let json = require('../../config.json')
+
+class Database {
+    constructor( config ) {
+        this.connection = mysql.createConnection(json);
+    }
+    query( sql, args ) {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.query( sql, args, ( err, rows ) => {
+                if ( err )
+                    return reject( err );
+                resolve( rows );
+            } );
+        } );
+    }
+    close() {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.end( err => {
+                if ( err )
+                    return reject( err );
+                resolve();
+            } );
+        } );
+    }
+}
+
+module.exports = {
+    Database
+}
